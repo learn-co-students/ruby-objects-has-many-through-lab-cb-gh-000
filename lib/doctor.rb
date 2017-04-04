ee2831aa0c7f4a45a035d25286b0c7d9bb8fcaad
Hiep Nguyen <tahi1990@gmail.com>
2017-04-04 08:46:31 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,17 @@
+class Doctor
+
+  attr_accessor :appointments, :patients
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @patients = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+end
