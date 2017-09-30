10341cb3596e83978672cc0b6ae67a6422e5d3da
Colin McKean <colinmckeandigital@gmail.com>
2017-09-30 11:27:49 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,17 @@
+class Genre
+  attr_accessor :title, :name
+  def initialize name
+    @name = name
+    @songs = []
+  end
+  def add_song song
+    @songs << song
+  end
+  def songs
+    @songs
+  end
+  def artists
+    self.songs.collect {|song| song.artist }
+
+  end
+end
