f65c346235f937f1f220989a97f284d87d87aa47
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-21 18:17:39 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+
+class Patient
+  
+  attr_reader :appointments,  :name 
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+  
+  def add_appointment(appointment)
+    self.appointments << appointment
+    appointment.patient = self
+  end
+  
+  def doctors 
+    self.appointments.collect { |appointment| appointment.doctor }
+  end
+  
+end 
+
+
+
+
