5e9121f234443f082e5de2603533b1a3230d2af7
Pranav Sharma <ps20maths@gmail.com>
2018-07-08 12:40:10 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,15 @@
+class Appointment
+  @@all =[]
+  attr_accessor :doctor , :patient, :date
+  def initialize(patient, doctor, date)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+  @@all =[]
+  attr_accessor :name , :artist , :songs
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs =[]
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artists
+    @songs.collect{|song| song.artist }
+  end
+
+end
